6d3e013c0841f3c5124dee4226d90deb52115cd2
Dan-M. Mitreanu <mitreanu@cityscroller.com>
2026-07-01 19:30:40 +0300

    add concurrency

--- SimpleCGI.WebServer/Program.cs
+++ SimpleCGI.WebServer/Program.cs
@@ -1,13 +1,27 @@
 ﻿
 using System.Net;
+using System.Threading.Channels;
 using SimpleCGI.Types;
 using SimpleCGI.WebServer;
 
+if (args.Length != 0 && args[0] == "help")
+{
+    Console.WriteLine("SimpleCGI.WebServer [host_url] [wwwroot_path] [concurrency] [request_queue_size]");
+    return;
+}
+
 string url = args.Length >= 1 ? args[0] : "http://localhost:8080";
 string wwwroot = args.Length >= 2 ? args[1] : Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+int concurrency = args.Length >= 3 ? int.Parse(args[2]) : Environment.ProcessorCount;
+int queueSize = args.Length >= 4 ? int.Parse(args[3]) : 10_000;
 
 if (!url.EndsWith('/'))
     url = $"{url}/";
+
+var requestChannel = Channel.CreateBounded<HttpListenerContext>(new BoundedChannelOptions(queueSize)
+{
+    FullMode = BoundedChannelFullMode.Wait
+});
 
 Sitemap sitemap = Sitemap.ReadDirectory(wwwroot);
 Router router = new(sitemap);
@@ -18,11 +32,14 @@ var ct = cts.Token;
 Console.CancelKeyPress += (sender, evt) =>
 {
     cts.Cancel();
+    requestChannel.Writer.TryComplete();
     evt.Cancel = true;
 };
 
 HttpListener listener = new();
 listener.Prefixes.Add(url);
+
+var workers = Enumerable.Range(0, concurrency).Select(_ => Task.Run(() => Process(router, requestChannel, ct))).ToArray();
 
 listener.Start();
 Console.WriteLine("Started at {0}", url);
@@ -34,17 +51,17 @@ while (!ct.IsCancellationRequested)
         var ctx = await listener.GetContextAsync().WaitAsync(ct);
         try
         {
-            var request = RequestType.FromHttpRequest(ctx.Request);
-            var routeResult = router.Route(request);
-            await Executor.Execute(routeResult, request, ctx.Response, ct);
+            await requestChannel.Writer.WriteAsync(ctx, ct);
         }
         catch (OperationCanceledException)
         {
             ctx.Response.StatusCode = 504;
+            ctx.Response.Close();
         }
-        finally
+        catch (Exception ex)
         {
-            ctx.Response.Headers["server"] = "SimpleCGI";
+            Console.Error.WriteLine("Failed to add request to queue {0}: {1}\n{2}", ex.GetType().Name, ex.Message, ex.StackTrace);
+            ctx.Response.StatusCode = 500;
             ctx.Response.Close();
         }
     }
@@ -55,5 +72,42 @@ while (!ct.IsCancellationRequested)
     catch (Exception ex)
     {
         Console.Error.WriteLine("Uncaught exception while accepting HTTP context {0}: {1}\n{2}", ex.GetType().Name, ex.Message, ex.StackTrace);
+    }
+}
+
+await Task.WhenAll(workers);
+
+static async Task Process(Router router, Channel<HttpListenerContext> chan, CancellationToken ct)
+{
+    try
+    {
+        await foreach (var ctx in chan.Reader.ReadAllAsync(ct))
+        {
+            try
+            {
+                var request = RequestType.FromHttpRequest(ctx.Request);
+                var routeResult = router.Route(request);
+                await Executor.Execute(routeResult, request, ctx.Response, ct);
+            }
+            catch (OperationCanceledException)
+            {
+                ctx.Response.StatusCode = 504;
+            }
+            catch (Exception ex)
+            {
+                ctx.Response.StatusCode = 500;
+                Console.Error.WriteLine("Uncaught exception while executing HTTP request {0}: {1}\n{2}", ex.GetType().Name, ex.Message, ex.StackTrace);
+            }
+            finally
+            {
+                ctx.Response.Headers["server"] = "SimpleCGI";
+                ctx.Response.Close();
+            }
+        }
+    }
+    catch (OperationCanceledException) {}
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine("Executor ended unexpectedly with exception {0}: {1}\n{2}", ex.GetType().Name, ex.Message, ex.StackTrace);
     }
 }
